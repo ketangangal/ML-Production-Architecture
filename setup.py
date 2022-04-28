cfbe803d746bf6e653081f9529bf36af75708a9c
ketanGangal <ketangangal98@gmail.com>
2022-04-28 15:24:38 +0530

    created setup.py for internal packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Production Architecture",
+    version="0.0.1",
+    author="Ketan Gangal",
+    author_email="ketangangal98@gmail.com",
+    packages=find_packages()
+)
