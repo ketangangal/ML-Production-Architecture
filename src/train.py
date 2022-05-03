61a385a1e680f64f0ea04cd8c4cbbfc47a11b2ee
ketanGangal <ketangangal98@gmail.com>
2022-05-03 11:49:30 +0530

    added exception handling and logging

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+from logging.logging import CustomLogger
+from exception.exception import CustomException
 from aws_feature_store.feature_store import FeatureStoreConnection
 from aws_model_registry.model_registry import ModelRegistryConnection
 from data_preprocessing_service.preprocessing import Preprocessing
@@ -9,6 +11,7 @@ from from_root import from_root
 from joblib import dump
 import os
 
+logger = CustomLogger("logs")
 
 class TrainModel:
     def __init__(self):
@@ -50,29 +53,33 @@ class TrainModel:
         mail.send_email()
 
     def train(self):
-        feature_data = FeatureStoreConnection(bucket_name=self.feature_store,key=self.raw_data_key)
-        raw_data = feature_data.get_features_from_s3()
+        try:
+            feature_data = FeatureStoreConnection(bucket_name=self.feature_store,key=self.raw_data_key)
+            raw_data = feature_data.get_features_from_s3()
 
-        print("Data fetched from feature Store")
+            print("Data fetched from feature Store")
 
-        preprocess = Preprocessing(df=raw_data,label=self.label,test_size=self.test_size,random_state=self.random_state)
-        X_train, X_test, y_train, y_test = preprocess.preprocess()
-        print(y_train.unique(), y_test.unique())
-        print("Preprocessing Completed!")
+            preprocess = Preprocessing(df=raw_data,label=self.label,test_size=self.test_size,random_state=self.random_state)
+            X_train, X_test, y_train, y_test = preprocess.preprocess()
+            print(y_train.unique(), y_test.unique())
+            print("Preprocessing Completed!")
 
-        accuracy, f1, recall = self.model(X_train, X_test, y_train, y_test)
+            accuracy, f1, recall = self.model(X_train, X_test, y_train, y_test)
 
-        print(f"Accuracy : {accuracy}")
-        print(f"f1_score : {f1}")
-        print(f"recall_score : {recall}")
+            print(f"Accuracy : {accuracy}")
+            print(f"f1_score : {f1}")
+            print(f"recall_score : {recall}")
 
-        registry = ModelRegistryConnection(bucket_name=self.model_registry,zip_files=self.zip_files,
-                                           package_name=self.package_name)
-        registry.upload_model_in_test()
-        print("Model Registered In the testing...")
-        #self.send_email()
+            registry = ModelRegistryConnection(bucket_name=self.model_registry,zip_files=self.zip_files,
+                                               package_name=self.package_name)
+            registry.upload_model_in_test()
+            print("Model Registered In the testing...")
+            # self.send_email()
 
-        return "Process Completed"
+            return "Process Completed"
+        except Exception as e:
+            message = CustomException(e, sys)
+            logger.error(message.error_message)
 
 
 if __name__ == "__main__":
