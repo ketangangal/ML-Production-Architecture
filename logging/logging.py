32597ebcbe834a2efbe36255f03495d23ed18f2a
ketanGangal <ketangangal98@gmail.com>
2022-05-03 11:48:41 +0530

    added custom logging

---
+++ logging/logging.py
@@ -0,0 +1,35 @@
+import os
+from datetime import datetime
+
+class CustomLogger:
+
+    def __init__(self, log_file_name):
+        """
+        :param log_file_name: name of the logfile
+        """
+        self.logfile = os.path.join(from_root(),"logging", log_file_name + str('.txt'))
+        self.current_date = str(datetime.now())
+
+    def info(self, message):
+        """
+        :arg message: message to write in the log file
+        """
+        try:
+            with open(self.logfile, 'a+') as logs:
+                logs.write(f'INFO [{self.current_date}]: {message}.\n')
+
+        except Exception as e:
+            with open(self.logfile, 'w') as logs:
+                logs.write(f'INFO [{self.current_date}]: {message}.\n')
+
+    def error(self, message):
+        """
+        :arg message: message to write in the log file
+        """
+        try:
+            with open(self.logfile, 'a+') as logs:
+                logs.write(f'ERROR [{self.current_date}]: {message}.\n')
+
+        except Exception as e:
+            with open(self.logfile, 'w') as logs:
+                logs.write(f'ERROR [{self.current_date}]: {message}.\n')
